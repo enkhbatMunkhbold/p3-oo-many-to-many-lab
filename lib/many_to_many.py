de50f69f1f79cef56d4a6ab6b000501c6b39d04e
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-08-29 23:14:56 -0700

    Current status

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,46 @@
-class Author:
-    pass
+from datetime import datetime
 
+class Author:
+    all = []
+
+    def __init__(self, name):
+        self.name = name  
+        
 
 class Book:
-    pass
+    all = []
+
+    def __init__(self, title):
+        self.title = title
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties) -> None:
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+
+    @property
+    def author(self):
+        return self.author
+    
+    @author.setter
+    def author(self, author):
+        if isinstance(author, Author):
+            self.author = author
+        else:
+            raise Exception("Instance must be Author class.")
+        
+    # @property
+    # def book(self):
+    #     return self.book
+    
+    # @book.setter
+    # def book(self, book):
+    #     if isinstance(book, Book):
+    #         self.book = book
+    #     else:
+    #         raise Exception("Instance must be Book class.")
